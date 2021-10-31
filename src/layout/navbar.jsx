7902ae025765dd7c8276c76078d4b2b19979e939
kawlik <kpawlik1103@gmail.com>
2021-10-31 12:56:38 +0100

    Navbar ok

--- src/layout/navbar.jsx
+++ src/layout/navbar.jsx
@@ -18,12 +18,27 @@ import AnnouncementIcon from '@mui/icons-material/Announcement';
 export default function Navbar() {
 
 
+    const [ isExpanded, setIsExpanded ] = useState( false );
+
+
+    function toggleIsExpanded() {
+        setIsExpanded( prev => !prev );
+    };
+
+    function expand() {
+        if( window.innerWidth > 767 ) { setIsExpanded( true ); }
+    };
+
+    function diminish() {
+        if( window.innerWidth > 767 || isExpanded ) { setIsExpanded( false ); }
+    };
+
 /*  Component layout
 /*   *   *   *   *   *   *   *   *   *   */
 
 return(
     <>
-        <nav id='navbar'>
+        <nav id='navbar' className={ isExpanded ? 'isExpanded' : '' } onMouseMove={ expand } onMouseEnter={ expand } onMouseLeave={ diminish } onClick={ toggleIsExpanded } >
 
             <header className='logo'>
                 <span className='text'>KONAR</span>
--- vite.config.js
+++ vite.config.js
@@ -32,9 +32,6 @@ export default defineConfig({
 
     // wsl dev server polling
     server: {
-
-        host: true,
-
         watch: {
             usePolling: true,
         }
